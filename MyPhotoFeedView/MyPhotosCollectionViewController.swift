761bfcf5f96ffdd46a308fe3a324d51329d6a0ed
Doug Harper <doug@zewkey.com>
2016-09-29 12:09:39 -0500

    Initial Commit
    Started project.
    Added empty CollectionViewController
    Added MyPhotoFeedVC class
    added t github

---
+++ MyPhotoFeedView/MyPhotosCollectionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MyPhotosCollectionViewController.swift
+//  MyPhotoFeedView
+//
+//  Created by doug harper on 9/29/16.
+//  Copyright © 2016 Doug Harper. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class MyPhotosCollectionViewController: UICollectionViewController {
+
+}
